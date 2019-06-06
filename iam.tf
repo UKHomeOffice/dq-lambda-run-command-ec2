371a1631cb8f2296349b446f42297d371aada9a9
Duncan Mountford <duncan9562@gmail.com>
2019-06-06 11:20:18 +0100

    Remove unused param

--- iam.tf
+++ iam.tf
@@ -34,7 +34,6 @@ resource "aws_iam_role_policy" "lambda-run-command-ec2-policy" {
       ],
       "Effect": "Allow",
       "Resource": [
-        "arn:aws:ssm:eu-west-2:*:parameter/tableau_server_username",
         "arn:aws:ssm:eu-west-2:*:parameter/ssh_key_private"
       ]
     }
